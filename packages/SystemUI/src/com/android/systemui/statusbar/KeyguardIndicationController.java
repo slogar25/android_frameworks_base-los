8dd98a4b75cce3572565c8cfe542bf3fddeeb625
Chris Crump <chriscrawford893@gmail.com>
2023-11-25 11:35:30 +0000

    fixup! base: Initial SenseProvider for FaceSense service
    Thanks to Tobi to figure out the fix for FaceSense needing a reboot to work after registering a face model.
    Also thanks to someone5678 for fixing compile on QPR1.
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Tobias Merkel <stylogey@gmail.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Change-Id: I1b83429a793a50fba292a38dbf5aa8a54ae5ace3
    Signed-off-by: basamaryan <basam.aryan@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -216,7 +216,7 @@ public class KeyguardIndicationController {
     private final Set<Integer> mCoExFaceAcquisitionMsgIdsToShow;
     private final FaceHelpMessageDeferral mFaceAcquiredMessageDeferral;
     private boolean mInited;
-    
+
     private boolean mFaceDetectionRunning;
 
     private KeyguardUpdateMonitorCallback mUpdateMonitorCallback;
@@ -250,7 +250,7 @@ public class KeyguardIndicationController {
                 mBiometricErrorMessageToShowOnScreenOn = null;
             }
         }
-        
+
         @Override
         public void onScreenTurnedOff() {
             if (mFaceDetectionRunning) {
@@ -1000,8 +1000,9 @@ public class KeyguardIndicationController {
     private void showFaceUnlockRecognizingMessage() {
         mFaceIconView.setVisibility(View.VISIBLE);
         mFaceIconView.setState(FaceUnlockImageView.State.SCANNING);
-        showBiometricMessage(mContext.getResources().getString(
-                                    R.string.face_unlock_recognizing));
+        String faceUnlockMessage = mContext.getResources().getString(
+            R.string.face_unlock_recognizing);
+        showBiometricMessage(faceUnlockMessage);
     }
 
     private void hideFaceUnlockRecognizingMessage() {
